700f1ca616e112b5d1949cb8476f07e79fd70d98
Alex-font <87749887+Alex-font@users.noreply.github.com>
2024-11-15 04:06:41 +0200

    Парамери файлу бібліотеки config.h
    Данний файл має знаходитись у тій же папці, де і основний код.

---
+++ config.h
@@ -0,0 +1,18 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Wi-Fi параметри
+const char* ssid = "назва вашої мережі";
+const char* password = "пароль до неї";
+
+// Параметри МІСТО та КРАЇНА
+const String CITY_NAME = "Kyiv"; // Назва міста
+const String COUNTRY_CODE = "UA";  // Код країни
+
+// Ваш API ключ для OpenWeatherMap
+const String API_KEY = "API_KEY";  // Вставте свій API ключ
+
+// Отримати API_KEY можна отримати безкоштовно після реєстрації на https://home.openweathermap.org
+
+#endif
