2f90dbf6a6dc8ffa262e512bdc08b0a677ed0309
Alex-font <87749887+Alex-font@users.noreply.github.com>
2024-11-25 20:19:37 +0200

    Update config.h
    додано запит погоди по визначеним координатах

--- config.h
+++ config.h
@@ -7,12 +7,18 @@ const char* ssid = "назва вашої мережі";
 const char* password = "пароль до неї";
 
 // Параметри МІСТО та КРАЇНА
-const String CITY_NAME = "Kyiv"; // Назва міста
-const String COUNTRY_CODE = "UA";  // Код країни
+//const String CITY_NAME = "Kyiv"; // Назва міста
+//const String COUNTRY_CODE = "UA";  // Код країни
+
+// АБО
+
+// Визначення координат для Вашого населеного пункту
+const float LATITUDE = 00.0000;  // Широта
+const float LONGITUDE = 00.0000;  // Довгота
 
 // Ваш API ключ для OpenWeatherMap
 const String API_KEY = "API_KEY";  // Вставте свій API ключ
 
 // Отримати API_KEY можна отримати безкоштовно після реєстрації на https://openweathermap.org
 
-#endif
+#endi
